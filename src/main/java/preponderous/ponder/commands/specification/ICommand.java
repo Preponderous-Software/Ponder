b2f5f130129993ca2b9c0ab42bfa716a3552fc3a
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-12 20:44:46 -0600

    Implemented Command.java

--- src/main/java/preponderous/ponder/commands/specification/ICommand.java
+++ src/main/java/preponderous/ponder/commands/specification/ICommand.java
@@ -1,8 +1,10 @@
 package preponderous.ponder.commands.specification;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public interface ICommand {
     boolean execute(CommandSender sender);
     boolean execute(CommandSender sender, String[] args);
+    boolean sendMessageIfNoArguments(String message, String[] args, CommandSender sender, ChatColor color);
 }
