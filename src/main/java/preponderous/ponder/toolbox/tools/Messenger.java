d75c6a2a6db3fae9784768267759fb5ed89771c8
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-15 21:21:11 -0600

    Allowed developers to send a message to players within a certain number of blocks from a player using the Messenger tool.

--- src/main/java/preponderous/ponder/toolbox/tools/Messenger.java
+++ src/main/java/preponderous/ponder/toolbox/tools/Messenger.java
@@ -1,8 +1,11 @@
 package preponderous.ponder.toolbox.tools;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import preponderous.ponder.toolbox.tools.specification.IMessenger;
+
+import static org.bukkit.Bukkit.getServer;
 
 public class Messenger implements IMessenger {
 
@@ -20,6 +23,35 @@ public class Messenger implements IMessenger {
         catch(Exception ignored) {
 
         }
+    }
+
+    /**
+     * Method to send a message to players within a certain number of blocks from a player.
+     *
+     */
+    @Override
+    public int sendMessageToPlayersWithinDistance(Player player, String message, int distance) {
+        Location playerLocation = player.getLocation();
+
+        int numPlayersWhoHeard = 0;
+
+        // for every online player
+        for (Player potentialPlayer : getServer().getOnlinePlayers()) {
+
+            // if not in world
+            if (potentialPlayer.getLocation().getWorld().getName() != playerLocation.getWorld().getName()) {
+                return -1;
+            }
+
+            // if not within the specified distance
+            if (!(potentialPlayer.getLocation().distance(playerLocation) < distance)) {
+                return -1;
+            }
+
+            numPlayersWhoHeard++;
+            potentialPlayer.sendMessage(message);
+        }
+        return numPlayersWhoHeard;
     }
 
 }
--- src/main/java/preponderous/ponder/toolbox/tools/specification/IMessenger.java
+++ src/main/java/preponderous/ponder/toolbox/tools/specification/IMessenger.java
@@ -1,5 +1,8 @@
 package preponderous.ponder.toolbox.tools.specification;
 
+import org.bukkit.entity.Player;
+
 public interface IMessenger {
     void sendAllPlayersOnServerMessage(String message);
+    int sendMessageToPlayersWithinDistance(Player player, String message, int distance);
 }
