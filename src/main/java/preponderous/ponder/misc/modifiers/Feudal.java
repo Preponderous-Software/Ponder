eee099833e2922d102d59f41d1451896c1c13d61
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-17 02:41:11 -0600

    Added Diplomatic.java and Feudal.java

---
+++ src/main/java/preponderous/ponder/misc/modifiers/Feudal.java
@@ -0,0 +1,26 @@
+package preponderous.ponder.misc.modifiers;
+
+import java.util.ArrayList;
+
+public interface Feudal {
+
+    // type
+    boolean isVassal(String name);
+    boolean isLiege();
+
+    // liege
+    void setLiege(String newLiege);
+    String getLiege();
+    boolean hasLiege();
+    boolean isLiege(String name);
+
+    // vassalage
+    void clearVassals();
+    int getNumVassals();
+    ArrayList<String> getVassals();
+    String getVassalsSeparatedByCommas();
+    void addAttemptedVassalization(String name);
+    boolean hasBeenOfferedVassalization(String name);
+    void removeAttemptedVassalization(String name);
+
+}
