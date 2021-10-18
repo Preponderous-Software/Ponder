86a752290218f9f64642a0a37efae64c076932bc
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-17 18:04:23 -0600

    Added the Lawful modifier.

---
+++ src/main/java/preponderous/ponder/modifiers/Lawful.java
@@ -0,0 +1,15 @@
+package preponderous.ponder.modifiers;
+
+import java.util.ArrayList;
+
+public interface Lawful {
+
+    // laws
+    void addLaw(String newLaw);
+    boolean removeLaw(String lawToRemove);
+    boolean removeLaw(int i);
+    boolean editLaw(int i, String newString);
+    int getNumLaws();
+    ArrayList<String> getLaws();
+
+}
