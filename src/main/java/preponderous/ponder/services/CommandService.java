a1033d68aa6c80fbf7e202ef734ebfafbf3e9ead
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-23 12:40:55 -0700

    Removed debug messages from CommandService.java

--- src/main/java/preponderous/ponder/services/CommandService.java
+++ src/main/java/preponderous/ponder/services/CommandService.java
@@ -39,43 +39,31 @@ public class CommandService {
      *
      */
     public boolean interpretCommand(CommandSender sender, String label, String[] args) {
-        ponder.log("Attempting to interpret command with label: " + label);
         if (!coreCommands.contains(label)) {
-            ponder.log("Command didn't match one of the core commands. Aborting.");
             return false;
         }
 
         if (args.length == 0) {
-            ponder.log("No arguments were given.");
             return false;
         }
 
         String subCommand = args[0];
-        ponder.log("Sub-command: " + subCommand);
 
         String[] arguments = ponder.getToolbox().getArgumentParser().dropFirstArgument(args);
-        ponder.log("Arguments: " + Arrays.toString(arguments));
-
-        ponder.log("Number of commands: " + commands.size());
 
         for (ICommand command : commands) {
-            ponder.log("Checking command " + command.getNames().get(0));
             if (command.getNames().contains(subCommand)) {
                 if (!ponder.getToolbox().getPermissionChecker().checkPermission(sender, command.getPermissions())) {
-                    ponder.log("The sender doesn't have permission to run this command.");
                     return false;
                 }
                 if (arguments.length == 0) {
-                    ponder.log("Executing with no additional arguments.");
                     return command.execute(sender);
                 }
                 else {
-                    ponder.log("Executing with additional arguments.");
                     return command.execute(sender, arguments);
                 }
             }
         }
-        ponder.log("The sub-command wasn't recognized.");
         sender.sendMessage(ChatColor.RED + notFoundMessage);
         return false;
     }
