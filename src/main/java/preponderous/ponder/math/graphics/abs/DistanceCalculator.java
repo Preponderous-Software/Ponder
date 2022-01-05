53f9b6f8259426e0d375f2983b67ea909ea81cfd
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-04 18:00:03 -0700

    Altered DistanceCalculator.java

--- src/main/java/preponderous/ponder/math/graphics/abs/DistanceCalculator.java
+++ src/main/java/preponderous/ponder/math/graphics/abs/DistanceCalculator.java
@@ -1,10 +1,12 @@
 package preponderous.ponder.math.graphics.abs;
 
 import preponderous.ponder.math.graphics.Point2D;
+import preponderous.ponder.math.graphics.Point3D;
 
 /**
  * @author Daniel Stephenson
  */
 public abstract class DistanceCalculator {
     public abstract double getDistanceBetweenPoints(Point2D firstPoint, Point2D secondPoint);
+    public abstract double getDistanceBetweenPoints(Point3D firstPoint, Point3D secondPoint);
 }
