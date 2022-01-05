1815958288b007ba1b7dc0b80fe94b729fcc5c7a
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-04 17:43:56 -0700

    Created Point3D.java

---
+++ src/main/java/preponderous/ponder/math/graphics/Point3D.java
@@ -0,0 +1,31 @@
+package preponderous.ponder.math.graphics;
+
+public class Point3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+}
