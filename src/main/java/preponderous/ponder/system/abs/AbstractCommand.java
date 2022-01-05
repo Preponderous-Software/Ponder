83a8a4169f405c677fa3c8de216e805de1fd2be5
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-05 14:14:07 -0700

    Created the system package.

---
+++ src/main/java/preponderous/ponder/system/abs/AbstractCommand.java
@@ -0,0 +1,9 @@
+package preponderous.ponder.system.abs;
+
+/**
+ * @author Daniel Stephenson
+ * @since January 5th, 2022
+ */
+public class AbstractCommand {
+    // TODO: implement
+}
---
+++ src/main/java/preponderous/ponder/system/services/ConfigService.java
@@ -0,0 +1,9 @@
+package preponderous.ponder.system.services;
+
+/**
+ * @author Daniel Stephenson
+ * @since January 5th, 2022
+ */
+public class ConfigService {
+    // TODO: implement
+}
