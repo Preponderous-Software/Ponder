50edda89f667ad3dea6ebbe40466185abd0cfd61
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-12 16:41:31 -0700

    Cleaned up some code in a few classes.

--- src/main/java/preponderous/ponder/misc/ArgumentParser.java
+++ src/main/java/preponderous/ponder/misc/ArgumentParser.java
@@ -15,33 +15,29 @@ import java.util.regex.Pattern;
 public class ArgumentParser {
 
     /**
-     * Method to drop the first argument from an Array of Strings. This may return an empty array.
-     *
      * @param args to modify.
      * @return Modified Array of Strings.
      * @throws IllegalArgumentException if the arguments given are invalid.
      */
     public String[] dropFirstArgument(String[] args) {
-        if (args == null || args.length == 0) {
-            throw new IllegalArgumentException("Arguments not valid.");
-        }
+        ensureArgumentsExist(args);
         String[] toReturn = new String[args.length - 1];
         System.arraycopy(args, 1, toReturn, 0, args.length - 1);
         return toReturn;
     }
 
     /**
-     * Method to get all arguments within Double Quotes.
-     *
      * @param args to compile and scan.
      * @return {@link ArrayList} of {@link String} which were surrounded by " or Double-Quotes.
      * @throws IllegalArgumentException if the arguments given are invalid.
      */
     public ArrayList<String> getArgumentsInsideDoubleQuotes(String[] args) {
-        if (args == null || args.length == 0) {
-            throw new IllegalArgumentException("Arguments not valid.");
-        }
-        final ArrayList<String> toReturn = new ArrayList<>();
+        ensureArgumentsExist(args);
+        return parseForArguments(args);
+    }
+
+    private ArrayList<String> parseForArguments(String[] args) {
+        ArrayList<String> toReturn = new ArrayList<>();
         final String argumentString = String.join(" ", args);
         final Matcher matcher = Pattern.compile("\"[^\"]*\"").matcher(argumentString);
         while (matcher.find()) {
@@ -50,11 +46,12 @@ public class ArgumentParser {
         return toReturn;
     }
 
-    /**
-     * Test harness to confirm the functionality of the Argument Parser class.
-     *
-     * @param args of the program.
-     */
+    private void ensureArgumentsExist(String[] args) {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException("Arguments not valid.");
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length == 0) {
             String input = "/test fly \"one\" \"two\" banana";
--- src/main/java/preponderous/ponder/misc/abs/Diplomatic.java
+++ src/main/java/preponderous/ponder/misc/abs/Diplomatic.java
@@ -12,101 +12,85 @@ import java.util.ArrayList;
 public interface Diplomatic {
 
     /**
-     * Adds a new ally to the list of allies.
      * @param name The name of the new ally.
      */
     void addAlly(String name);
 
     /**
-     * Removes an ally from the list of allies.
      * @param name The name of the ally to remove.
      */
     void removeAlly(String name);
 
     /**
-     * Checks whether a ally exists.
      * @param name The name of the potential ally.
      * @return Whether the ally exists.
      */
     boolean isAlly(String name);
 
     /**
-     * This can be utilized to access the list of allies.
      * @return The list of allies.
      */
     ArrayList<String> getAllies();
 
     /**
-     * This can be used to get all the allies separated by comments in a string.
      * @return Allies separated by comments.
      */
     String getAlliesSeparatedByCommas();
 
     /**
-     *  This can be used to add a name to the list of requested allies.
      * @param name The name of the future ally.
      */
     void requestAlly(String name);
 
     /**
-     * Checks whether a name is in the list of requested allies.
      * @param name The potential requested ally.
      * @return Whether the name is in the list of requested allies.
      */
     boolean isRequestedAlly(String name);
 
     /**
-     * Removes a name from the list of requested allies.
      * @param name The name to remove from the list of requested allies.
      */
     void removeAllianceRequest(String name);
 
     /**
-     * Adds a name to the list of enemies.
      * @param name The name to add to the list of enemies.
      */
     void addEnemy(String name);
 
     /**
-     * Removes a name from the list of enemies.
      * @param name The name to remove from the list of enemies.
      */
     void removeEnemy(String name);
 
     /**
-     * Checks whether a name is in the list of enemies.
      * @param name The name to check.
      * @return Whether the name is in the list of enemies.
      */
     boolean isEnemy(String name);
 
     /**
-     * This can be used to access the list of enemies.
      * @return The list of enemies.
      */
     ArrayList<String> getEnemies();
 
     /**
-     * This can be used to get all of the enemies separated by comments in a string.
      * @return Enemies separated by comments.
      */
     String getEnemiesSeparatedByCommas();
 
     /**
-     * Adds a name to the list of requested truces.
      * @param name The name to add to the list of requested truces.
      */
     void requestTruce(String name);
 
     /**
-     * Checks whether a truce is requested.
      * @param name The name to check.
      * @return Whether a truce is requested with the entity associated with the passed name.
      */
     boolean isTruceRequested(String name);
 
     /**
-     * Removes a name from the list of requested truces.
      * @param name The name to remove from the list of requested truces
      */
     void removeRequestedTruce(String name);
