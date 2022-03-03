9972336e35924249a48501a562772122362b3ede
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-03-03 13:59:47 +0700

    add MaterialChecker, update BlockChecker for older version can use it, update api to 1.18.2

---
+++ src/main/java/preponderous/ponder/minecraft/bukkit/tools/MaterialChecker.java
@@ -0,0 +1,44 @@
+package preponderous.ponder.minecraft.bukkit.tools;
+
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Daniel McCoy Stephenson
+ * @author VoChiDanh
+ */
+public class MaterialChecker {
+
+    public boolean isCoal(@NotNull Material material) {
+        return material.name().contains("COAL");
+    }
+
+    public boolean isIron(@NotNull Material material) {
+        return material.name().contains("IRON");
+    }
+
+    public boolean isGold(@NotNull Material material) {
+        return material.name().contains("GOLD");
+    }
+
+    public boolean isRedstone(@NotNull Material material) {
+        return material.name().contains("REDSTONE");
+    }
+
+    public boolean isLapis(@NotNull Material material) {
+        return material.name().contains("LAPIS");
+    }
+
+    public boolean isDiamond(@NotNull Material material) {
+        return material.name().contains("DIAMOND");
+    }
+
+    public boolean isEmerald(@NotNull Material material) {
+        return material.name().contains("EMERALD");
+    }
+
+    public boolean isLog(@NotNull Material material) {
+        return material.name().contains("LOG");
+    }
+
+}
